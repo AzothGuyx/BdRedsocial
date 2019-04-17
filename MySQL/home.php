cfb858b9f63c8fd845f4f609bffc190139196320
sentrak <santiago.jimenez.raigosa@hotmail.com>
2019-04-17 12:30:36 -0500

    hi

--- MySQL/home.php
+++ MySQL/home.php
@@ -5,7 +5,6 @@
 <title>Home Practica - Cassandra</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="./style.css" type="text/css" media="all" />
-<script src="./main.js"></script>
 </head>
 <body>
 <H1 class="mi_color">MySQL o MariaDB-Home</H1>
@@ -68,32 +67,7 @@ if ($mysqli->connect_errno) {
 		</table>';
 	?>
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
+|
 <H3>Home</H3>
 	<!-- Sin Filtro por fecha -->
 	<form name="q1" action="home.php" method="get">
