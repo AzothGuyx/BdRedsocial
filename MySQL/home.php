13fde5a3da59295a4f8278ab9ed6d61f5501a761
sentrak <santiago.jimenez.raigosa@hotmail.com>
2019-04-17 12:03:37 -0500

    hola

--- MySQL/home.php
+++ MySQL/home.php
@@ -5,10 +5,11 @@
 <title>Home Practica - Cassandra</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="./style.css" type="text/css" media="all" />
+<script src="./main.js"></script>
 </head>
 <body>
 <H1 class="mi_color">MySQL o MariaDB-Home</H1>
-<div>
+
 <!--Cosas bases-->
 <?PHP
 /*Se recuperan los argumentos*/
@@ -37,11 +38,6 @@ if ($mysqli->connect_errno) {
 }
 ?>
 
-
-
-
-   
-
 <?php
 //consulta los eventos que existe basado en # deasistentes-fecha-descripcion
 	$query = 'SELECT COUNT(A.evento_id), feevento, dsevento
@@ -53,7 +49,8 @@ if ($mysqli->connect_errno) {
 	echo '<table class="table">
 	<thead class="thead-dark">
 	  <tr>
-		<th scope="col">Numero de asistentes</th>
+	  <th scope="col">Asistir al evento</th>
+	  <th scope="col">Numero de asistentes</th>
 		<th scope="col">Fecha del evento</th>
 		<th scope="col">Descripcion del eventoast</th>
 	  </tr>
@@ -61,6 +58,7 @@ if ($mysqli->connect_errno) {
 	<tbody>';
 	foreach ($result as $row) {
 		echo '<tr>
+		<td><button type="button" class="btn btn-primary">Asistir</button></rd>
 		<td>'.$row['COUNT(A.evento_id)'].'</rd>
 		<td>'.$row['feevento'].'</td>
 		<td>'.$row['dsevento'].'</td>
@@ -69,6 +67,33 @@ if ($mysqli->connect_errno) {
 	echo '</tbody>
 		</table>';
 	?>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <H3>Home</H3>
 	<!-- Sin Filtro por fecha -->
 	<form name="q1" action="home.php" method="get">
