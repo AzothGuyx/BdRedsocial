fa3b1b5023b095959fb84d86c4ff2bd933b8a588
sentrak <santiago.jimenez.raigosa@hotmail.com>
2019-04-18 11:02:23 -0500

    por fin

--- MySQL/home.php
+++ MySQL/home.php
@@ -3,10 +3,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Home Practica - Cassandra</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-
-
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="all" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
 </head>
 <body>
 <H1 class="mi_color">MySQL o MariaDB-Home</H1>
@@ -89,28 +94,65 @@ WHERE login ='.'\''.$login.'\'';
 
 //Consultar la agenda mediante el boton consultar.
 ?>	
-<div class="container">
-  <h2>Ejecutar Acción al Cerrar Modal</h2>
-  <p>
-  Probaremos como ejecutar una accion al detectar el cierre del modal
-  </p>
-  <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-md" id="myBtn">Ocultar Modal</button>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Ejecutar Acción al Cerrar Modal</h4>
-        </div>
-        <div class="modal-body">
-          <p>Probaremos como ejecutar una accion al detectar el cierre del modal</p>
-        </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+	  <!-- Titulo del pop up -->
+        <h5 class="modal-title" id="exampleModalLabel">Mi agenda</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-      
+	  <!-- Cuerpo del pop up -->
+      <div class="modal-body">
+		<?php
+		$query = 'SELECT A.evento_id as "idEvento", COUNT(A.evento_id), E.feevento, E.dsevento
+		FROM agenda A
+		INNER JOIN evento E ON E.id = A.evento_id
+		GROUP BY A.evento_id';
+	
+		$result = $mysqli->query($query);
+		echo '<table class="table">
+		<thead class="thead-dark">
+		  <tr>
+		  <th scope="col">Asistir al evento</th>
+		  <th scope="col">Numero de asistentes</th>
+			<th scope="col">Fecha del evento</th>
+			<th scope="col">Descripcion del eventoast</th>
+		  </tr>
+		</thead>
+		<tbody>';
+		foreach ($result as $row) {
+			echo '
+	
+			<tr>
+			<td>
+			<form method="get" action="agendar.php">
+			<input type="hidden" name="idUsuario" value="'.$idUsuario.'">
+			<input type="hidden" name="idEvento" value="'.$row['idEvento'].'">
+			<button type="submit" class="btn btn-primary">asistir</button></form>
+			</td>
+			<td>'.$row['COUNT(A.evento_id)'].'</td>
+			<td>'.$row['feevento'].'</td>
+			<td>'.$row['dsevento'].'</td>
+		  </tr>';
+		}
+		echo '</tbody>
+			</table>';
+		?>
+      </div>
+	  <!-- pie del pop up -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
   </div>
 </div>
@@ -134,6 +176,5 @@ WHERE login ='.'\''.$login.'\'';
 <?php
 $mysqli->close();
 ?>
-<script src="js/main.js"></script>
 </body>
 </html>
