6b872f18a99e97a5ef1ba5c01fd07ea34e492372
sentrak <santiago.jimenez.raigosa@hotmail.com>
2019-04-19 11:01:25 -0500

    s

--- MySQL/home.php
+++ MySQL/home.php
@@ -1,7 +1,6 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta charset="UTF-8">
 	<title>Home Practica - Cassandra</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="all" />
@@ -9,12 +8,9 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 </head>
-
 <body>
 	<H1 class="mi_color">MySQL o MariaDB-Home</H1>
-
 <!--Cosas bases-->
 	<?PHP
 /*Se recuperan los argumentos*/
@@ -89,7 +85,7 @@ Permitir agendar uno de estos eventos mediante el boton asistir.
            			 foreach ($result as $row) {
 						echo '<tr>
 								<td>
-									<button type="button" class="btn btn-primary" onClick="MiFuncionJS();">Asistir</button>
+									<button type="button" class="btn btn-primary"><?php echo "hello world";?>Asistir</button>
 								</td>
 								<td>'.$row['COUNT(A.evento_id)'].'</td>
 								<td>'.$row['feevento'].'</td>
@@ -162,23 +158,20 @@ Actualizar el numero de likes de una publicacón agregandole 1.-->
 				<tbody>
 					<?php
 						$query = 'SELECT L.numLikes as "like", P.dspublicacion as "pub", P.id as "idP" FROM publicacion P INNER JOIN likes L ON P.id = L.publicacion_id';
-						$result = $mysqli->query($query);
+							$result = $mysqli->query($query);
 						foreach ($result as $row) {
 							echo '
 							<tr>
-							<td>
-							<button type="button" class="btn btn-primary" onclick="like.php?id='.$row['idP'].'">
-							<img src="img/like_on" width="15px" heigh="15px"> Like</button>
-							</td>
-							<td>'.$row['like'].'</td>
-							<td>'.$row['pub'].'</td>
-						</tr>';
+								<td><button type="button" class="btn btn-primary" onclick="like.php?id='.$row['idP'].'"><img src="img/like_on" width="15px" heigh="15px"> Like</button></td>
+								<td>'.$row['like'].'</td>
+								<td>'.$row['pub'].'</td>
+							</tr>';
 						}
 					?>
 				</tbody>
 			</table>
 		</article>
-<!-- HDA#4:
+<!-- HDA #4:
 Insertar una nueva publicacion en el grupo principal del usuario-->
 		<article id="btnNuevaP">
 			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#CrearPublicacion">Crear publicación</button>
@@ -187,19 +180,14 @@ Insertar una nueva publicacion en el grupo principal del usuario-->
 		</article>
 	</section>
 	</section>
-	
 
-<script language="javascript" type="text/javascript">
-function MiFuncionJS()
-{  
+<!-- HDA #5:
+visalizar la información del usuario que ingresó
+con los campos de Usuario, grupo principal y otros grupos.-->
+	<footer>
+		
+	</footer>
 
-	alert ("EXITO");
-	<?php
-		echo "hola mundo";
-		?>
-}
-
-</script>
 	<?php
 		$mysqli->close();
 	?>
